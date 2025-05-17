9dbbe03917bfeb3e896273509fa931a887119cdf
mantech <franciscollins42@gmail.com>
2025-05-17 17:39:15 +0100

    keep on

--- foodstore-site/vite.config.js
+++ foodstore-site/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // 👈 Netlify expects this directory
-  }
+  
 })
 
